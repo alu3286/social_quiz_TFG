6daa76afffb8878d2505f549b0794e4be3766e34
Eduardo <eaculed@gmail.com>
2015-04-20 21:19:36 +0100

    Modificando la vista de creacion de examenes

--- public/js/jquery.bootstrap-duallistbox.js
+++ public/js/jquery.bootstrap-duallistbox.js
@@ -4,7 +4,7 @@
     defaults = {
       bootstrap2Compatible: false,
       filterTextClear: 'show all',
-      filterPlaceHolder: 'Filter',
+      filterPlaceHolder: 'Filtro',
       moveSelectedLabel: 'Move selected',
       moveAllLabel: 'Move all',
       removeSelectedLabel: 'Remove selected',
@@ -20,7 +20,7 @@
       selectedFilter: '',                                                                 // string, filter the selected options
       infoText: 'Showing all {0}',                                                        // text when all options are visible / false for no info text
       infoTextFiltered: '<span class="label label-warning">Filtered</span> {0} from {1}', // when not all of the options are visible due to the filter
-      infoTextEmpty: 'Empty list',                                                        // when there are no options present in the list
+      infoTextEmpty: 'Lista vacía',                                                        // when there are no options present in the list
       filterOnValues: false                                                               // filter by selector's values, boolean
     },
     // Selections are invisible on android if the containing select is styled with CSS
