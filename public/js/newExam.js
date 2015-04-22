46b998c8943e7b44f1e8b0188eeae32f0863247e
Eduardo <eaculed@gmail.com>
2015-04-22 14:04:39 +0100

    Trabajando con el formulario de examenes

--- public/js/newExam.js
+++ public/js/newExam.js
@@ -1,68 +1,40 @@
-$(document).ready(function() 
-{
-$('.add').click(function(){
+$(function () {
 
-    $('.all').prop("checked",false);
-    var items = $("#list1 input:checked:not('.all')");
-    var n = items.length;
-    if (n > 0) {
-      items.each(function(idx,item){
-        var choice = $(item);
-        choice.prop("checked",false);
-        choice.parent().appendTo("#list2");
-      });
-    }
-    else {
-      alert("Elige un elemento de la primera lista");
-    }
-});
-
-$('.remove').click(function(){
- 
-    $('.all').prop("checked",false);
-    var items = $("#list2 input:checked:not('.all')");
-  items.each(function(idx,item){
-      var choice = $(item);
-      choice.prop("checked",false);
-      choice.parent().appendTo("#list1");
+    $('body').on('click', '.list-group .list-group-item', function () {
+        $(this).toggleClass('active');
     });
-});
-
-/* toggle all checkboxes in group */
-$('.all').click(function(e){
-  
-  e.stopPropagation();
-  var $this = $(this);
-    if($this.is(":checked")) {
-      $this.parents('.list-group').find("[type=checkbox]").prop("checked",true);
-    }
-    else {
-      $this.parents('.list-group').find("[type=checkbox]").prop("checked",false);
-        $this.prop("checked",false);
-    }
-});
-
-$('[type=checkbox]').click(function(e){
-
-  e.stopPropagation();
-});
-
-/* toggle checkbox when list group item is clicked */
-$('.list-group a').click(function(e){
-   
-    e.stopPropagation();
-  
-    var $this = $(this).find("[type=checkbox]");
-    if($this.is(":checked")) {
-      $this.prop("checked",false);
-    }
-    else {
-      $this.prop("checked",true);
-    }
-  
-    if ($this.hasClass("all")) {
-      $this.trigger('click');
-    }
-});
+    $('.list-arrows button').click(function () {
+        var $button = $(this), actives = '';
+        if ($button.hasClass('move-left')) {
+            actives = $('.list-right ul li.active');
+            actives.clone().appendTo('.list-left ul');
+            actives.remove();
+        } else if ($button.hasClass('move-right')) {
+            actives = $('.list-left ul li.active');
+            actives.clone().appendTo('.list-right ul');
+            actives.remove();
+        }
+    });
+    $('.dual-list .selector').click(function () {
+        var $checkBox = $(this);
+        if (!$checkBox.hasClass('selected')) {
+            $checkBox.addClass('selected').closest('.well').find('ul li:not(.active)').addClass('active');
+            $checkBox.children('i').removeClass('glyphicon-unchecked').addClass('glyphicon-check');
+        } else {
+            $checkBox.removeClass('selected').closest('.well').find('ul li.active').removeClass('active');
+            $checkBox.children('i').removeClass('glyphicon-check').addClass('glyphicon-unchecked');
+        }
+    });
+    $('[name="SearchDualList"]').keyup(function (e) {
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        if (code == '27') $(this).val(null);
+        var $rows = $(this).closest('.dual-list').find('.list-group li');
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+        $rows.show().filter(function () {
+            var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+            return !~text.indexOf(val);
+        }).hide();
+    });
 
 });
