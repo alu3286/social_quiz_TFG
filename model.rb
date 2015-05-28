c7383bc8f9264cccdfbdd0f41de905254d3ed22f
Eduardo <eaculed@gmail.com>
2015-05-28 14:01:57 +0100

    Cambiando modelo para heroku

--- model.rb
+++ model.rb
@@ -6,86 +6,86 @@ require 'bcrypt'
 #DB = Sequel.sqlite('myquiz.db') # no memory database (en local)
 DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://database.db')
 
-DB.create_table! :usuario_grupo do
-  primary_key   [:idGrupo, :idUsuario]
-  foreign_key   :idGrupo, :grupos
-  foreign_key   :idUsuario, :usuarios
-end
+# DB.create_table! :usuario_grupo do
+#   primary_key   [:idGrupo, :idUsuario]
+#   foreign_key   :idGrupo, :grupos
+#   foreign_key   :idUsuario, :usuarios
+# end
 
-DB.create_table! :grupos do
-  primary_key   :idGrupo
-  String        :nombre
-  String        :descripcion
-  DateTime      :fecha_creacion
-  foreign_key   :idUsuario, :usuarios
-end
+# DB.create_table! :grupos do
+#   primary_key   :idGrupo
+#   String        :nombre
+#   String        :descripcion
+#   DateTime      :fecha_creacion
+#   foreign_key   :idUsuario, :usuarios
+# end
 
-DB.create_table! :respuestas do
-  primary_key   :idRespuesta
-  String        :texto
-  Boolean       :correcto
-  String        :tipo
-  foreign_key   :idPregunta, :preguntas
-end
+# DB.create_table! :respuestas do
+#   primary_key   :idRespuesta
+#   String        :texto
+#   Boolean       :correcto
+#   String        :tipo
+#   foreign_key   :idPregunta, :preguntas
+# end
 
-DB.create_table! :examen_pregunta do
-  primary_key   :idExamen
-  primary_key   :idPregunta
-  Float         :peso
-  Integer       :obligatoria
-  foreign_key   :idExamen, :examenes
-  foreign_key   :idPregunta, :preguntas
-end
+# DB.create_table! :examen_pregunta do
+#   primary_key   :idExamen
+#   primary_key   :idPregunta
+#   Float         :peso
+#   Integer       :obligatoria
+#   foreign_key   :idExamen, :examenes
+#   foreign_key   :idPregunta, :preguntas
+# end
 
-DB.create_table! :usuario_examen do
-  primary_key   :idUsuario
-  primary_key   :idExamen
-  primary_key   :intento
-  Time          :tiempo
-  Float         :nota
-  Integer       :numero_fallo
-  Float         :puntuacion
-  String        :titulo
-  DateTime      :fecha
-  foreign_key   :idUsuario, :usuarios
-  foreign_key   :idExamen, :examenes
-end
+# DB.create_table! :usuario_examen do
+#   primary_key   :idUsuario
+#   primary_key   :idExamen
+#   primary_key   :intento
+#   Time          :tiempo
+#   Float         :nota
+#   Integer       :numero_fallo
+#   Float         :puntuacion
+#   String        :titulo
+#   DateTime      :fecha
+#   foreign_key   :idUsuario, :usuarios
+#   foreign_key   :idExamen, :examenes
+# end
 
-DB.create_table! :usuario_examen_respuesta do
-  primary_key   :idUsuario
-  primary_key   :idExamen
-  primary_key   :idRespuesta
-  primary_key   :idPregunta
-  primary_key   :intento
-  String        :texto
+# DB.create_table! :usuario_examen_respuesta do
+#   primary_key   :idUsuario
+#   primary_key   :idExamen
+#   primary_key   :idRespuesta
+#   primary_key   :idPregunta
+#   primary_key   :intento
+#   String        :texto
 
-  foreign_key   :idUsuario, :usuario_examen
-  foreign_key   :idExamen, :usuario_examen
-  foreign_key   :intento, :usuario_examen
+#   foreign_key   :idUsuario, :usuario_examen
+#   foreign_key   :idExamen, :usuario_examen
+#   foreign_key   :intento, :usuario_examen
 
-  #foreign_key   :idExamen, :examen_pregunta
-  #foreign_key   :idPregunta, :examen_pregunta
+#   #foreign_key   :idExamen, :examen_pregunta
+#   #foreign_key   :idPregunta, :examen_pregunta
 
-  foreign_key   :idRespuesta, :respuestas
-  foreign_key   :idPregunta, :respuestas
-end
+#   foreign_key   :idRespuesta, :respuestas
+#   foreign_key   :idPregunta, :respuestas
+# end
 
-DB.create_table! :preguntas do
-  primary_key   :idPregunta
-  String        :titulo
-  DateTime      :fecha_creacion
-  String        :tags
-  foreign_key   :idUsuario, :usuarios
-end
+# DB.create_table! :preguntas do
+#   primary_key   :idPregunta
+#   String        :titulo
+#   DateTime      :fecha_creacion
+#   String        :tags
+#   foreign_key   :idUsuario, :usuarios
+# end
 
-DB.create_table! :examenes do
-  primary_key   :idExamen
-  String        :titulo
-  DateTime      :fecha_creacion
-  DateTime      :fecha_apertura
-  DateTime      :fecha_cierre
-  foreign_key   :idUsuario, :usuarios
-end
+# DB.create_table! :examenes do
+#   primary_key   :idExamen
+#   String        :titulo
+#   DateTime      :fecha_creacion
+#   DateTime      :fecha_apertura
+#   DateTime      :fecha_cierre
+#   foreign_key   :idUsuario, :usuarios
+# end
 
 DB.create_table! :usuarios do
   primary_key 	:idUsuario
